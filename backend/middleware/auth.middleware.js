46369bf4ffb8ccfe74176176b64a6d9a3306241c
ChamlingUttam <110346114+ChamlingUttam@users.noreply.github.com>
2026-02-22 15:44:44 +0545

    Enable JWT authentication in protectRoute middleware
    Uncommented the protectRoute middleware implementation for JWT authentication.

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,37 +1,3 @@
-// import  jwt  from "jsonwebtoken"
-// import User from "../models/user.model.js"
-
-// export const protectRoute = async(req,res,next)=>{
-
-//     try {
-//         const token = req.cookies.jwt
-
-//         if(!token){
-//             return res.status(401).json("no token provided")
-//         }
-
-//         const decode = jwt.verify(token, process.env.JWT_SECRET)
-
-//         if(!decode){
-//             return res.status(401).json("invalid token")
-//         }
-
-//         const user = await User.findById(decode.userId).select("-password")
-
-//         req.user = user
-
-//         next()
-        
-
-
-        
-//     } catch (error) {
-//         res.status(500).json({message:error.message})
-//     }
-// }
-
-
-
 
 
 import jwt from "jsonwebtoken";
